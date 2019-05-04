6f56d9946bb3dfa3e6a9f97005b65aea08f8df94
mateo2411 <47614981+mateo2411@users.noreply.github.com>
2019-05-04 10:31:25 -0500

    Create LibMatriz.h

---
+++ LibMatriz.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstring>
+#include <math.h>
+#include <cstdlib>
+#include <time.h> 
+
+void MatrizAleatoria(int trama[20]);
+void cipherData(int Num_alea[20], int ronda);
+void ValoresMatriz(int Matriz[20]);
